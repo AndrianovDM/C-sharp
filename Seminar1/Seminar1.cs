3e9d55bda1b3751c29e585da794e0064edb33dc7
Dmitry <andrianov1997@mail.ru>
2022-04-17 03:51:20 +0300

    add task 3

--- Seminar1/Seminar1.cs
+++ Seminar1/Seminar1.cs
@@ -21,3 +21,30 @@ else
 {
     Console.Write("First number is not a quad of second number ");
 }*/
+
+/*Задача №3*/
+
+/*Console.Write("input number of weekday:");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day > 7 || day < 1)
+{
+   Console.Write("Incorret number of day!"); 
+}
+else
+{
+    if (day == 1)
+       Console.Write("It`s Mondey"); 
+    if (day == 2)
+       Console.Write("Its Tuesday"); 
+    if (day == 3)
+       Console.Write("Its Wednesday"); 
+    if (day == 4)
+       Console.Write("Its Thursday"); 
+    if (day == 5)
+       Console.Write("Its Friday");
+    if (day == 6)
+       Console.Write("Its Saturday");
+    if (day == 7)
+       Console.Write("Its Sunday");   
+}*/
