5b8dbd91189640e57e3352bb6b207f9fd9665465
Dmitry <andrianov1997@mail.ru>
2022-04-17 15:30:08 +0300

    add task 3

--- Lecture1/Lecture1.cs
+++ Lecture1/Lecture1.cs
@@ -7,7 +7,15 @@ Console.Write(username); */
 
 /*Задача №2*/
 
-int numberA = 31;
+/*int numberA = 31;
 int numberB = 5;
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(result);*/
+
+/*Задача №3*/
+
+double numberA = 31;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+
