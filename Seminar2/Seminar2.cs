a1b823d27faf73371fdbfdff86fff3f6aac2a4b5
Dmitry <andrianov1997@mail.ru>
2022-04-18 02:25:12 +0300

    add task 1.2

--- Seminar2/Seminar2.cs
+++ Seminar2/Seminar2.cs
@@ -21,7 +21,7 @@ ShowNumber();
 */
 
 /*Задача №1.1*/
-
+/*
 int ShowNumber()
 {
     int num = new Random().Next(10,99);
@@ -40,4 +40,28 @@ int ShowNumber()
         }
 }
 int a = ShowNumber();
+Console.WriteLine(a);
+*/
+
+/*Задача №1.2*/
+
+int ShowNumber(int n)
+{
+    int firtsNum = n / 10;
+    int secondNum = n % 10;
+
+    Console.Write(n + " -> ");
+
+    if (firtsNum > secondNum)
+        {
+            return firtsNum;
+        }
+    else
+        {
+          return secondNum;  
+        }
+}
+    
+int num = new Random().Next(10,99);
+int a = ShowNumber(num);
 Console.WriteLine(a);
