4215cd0850c23c8074f795ad98b3aee2bf82b1f0
Dmitry <andrianov1997@mail.ru>
2022-04-17 15:46:26 +0300

    add task 5

--- Lecture1/Lecture1.cs
+++ Lecture1/Lecture1.cs
@@ -20,11 +20,28 @@ Console.WriteLine(numberA / numberB);*/
 
 /*Задача №4*/
 
-int numberA = new Random().Next(1,10); //создает рандомное число от 1 2 3 4...9
+/*int numberA = new Random().Next(1,10); //создает рандомное число от 1 2 3 4...9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10); 
 Console.WriteLine(numberB);
 int.result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(result);*/
+
+/*Задача №5*/
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
+
 
 
