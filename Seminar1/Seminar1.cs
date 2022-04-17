43d1c757fc66df50d9cdfceb003f125f31e1d8b7
Dmitry <andrianov1997@mail.ru>
2022-04-17 03:49:43 +0300

    add task 2

--- Seminar1/Seminar1.cs
+++ Seminar1/Seminar1.cs
@@ -5,3 +5,19 @@ int num = Convert.ToInt32(Console.ReadLine());
 //Convert.ToInt32 - Convert - класс конвертанции ToInt32 string в int 32 бита
 int result = num * num;
 Console.Write("Results is " + result);*/
+
+/*Задача №2*/
+
+/*Console.Write("input first integer number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second integer number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quad = num2 * num2;
+if (num1 == quad)
+{
+    Console.Write("First number is a quad of second number ");
+}
+else
+{
+    Console.Write("First number is not a quad of second number ");
+}*/
