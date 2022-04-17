106f83c71ccd5c62af7e1f610c372ad793565090
Dmitry <andrianov1997@mail.ru>
2022-04-18 02:23:35 +0300

    add task 1

--- Seminar2/Seminar2.cs
+++ Seminar2/Seminar2.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №1*/
+
+void ShowNumber()
+{
+    int num = new Random().Next(10,99);
+    int firtsNum = num / 10;
+    int secondNum = num % 10;
+
+    Console.Write(num + " -> ");
+
+    if (firtsNum > secondNum)
+        {
+            Console.WriteLine(firtsNum);
+        }
+    else
+        {
+          Console.WriteLine(secondNum);  
+        }
+}
+ShowNumber();
+
