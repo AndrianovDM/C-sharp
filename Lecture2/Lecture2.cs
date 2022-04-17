6e9a6c0f9e1ff1e6e68c092ffc6b0178cfd9581d
Dmitry <andrianov1997@mail.ru>
2022-04-17 18:09:30 +0300

    add task 5

--- Lecture2/Lecture2.cs
+++ Lecture2/Lecture2.cs
@@ -85,6 +85,7 @@ Console.WriteLine(result);
 
 /* Задача №4 */
 
+/*
 int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
 
 int n = array.Length;
@@ -101,5 +102,57 @@ while (index < n)
     }
     index++;
 }
+*/
+
+
+/* Задача №5 */
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создает массив в котором 10 элементов по умолчанию
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
 
 
