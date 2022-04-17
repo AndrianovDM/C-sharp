4fe99e13631b4d10885128e318e407ad9f41b31f
Dmitry <andrianov1997@mail.ru>
2022-04-17 04:01:13 +0300

    add task 2

--- Homework1/Homework1.cs
+++ Homework1/Homework1.cs
@@ -1,7 +1,7 @@
 ﻿/*Задача 1: 
 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
 
-Console.Write("Input integer first number: ");
+/*Console.Write("Input integer first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input integer second number: ");
@@ -20,4 +20,31 @@ else if(num1 < num2)
 else 
 {
    Console.Write("the number " + num1 + " = " + num2);  
+}*/
+
+
+/*Задача 2: 
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+Console.Write("Input integer first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input integer third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 >= num2 && num1 >= num3) 
+{
+    Console.Write("max number = " + num1);    
+}
+
+else if (num2 >= num1 && num2 >= num3) 
+{
+    Console.Write("max number = " + num2);    
+}
+else if (num3 >= num1 && num3 >= num2) 
+{
+    Console.Write("max number = " + num3);    
 }
