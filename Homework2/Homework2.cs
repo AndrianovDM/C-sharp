15c01f853508a3addf18a6e69af7dcfe73d06846
Dmitry <andrianov1997@mail.ru>
2022-04-19 00:53:25 +0300

    add task 1

--- Homework2/Homework2.cs
+++ Homework2/Homework2.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10:
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+*/
+
+void ShowNumber(int num)
+{
+    int number = (num / 10) % 10;
+    Console.Write(num + " -> " + number);
+}
+
+Console.Write("Input integer number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNumber(n);
+
+
