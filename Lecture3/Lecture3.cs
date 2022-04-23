d133135dbd711d94b1abe636958e8337e81cd73e
Dmitry <andrianov1997@mail.ru>
2022-04-23 16:55:32 +0300

    add Example 2

--- Lecture3/Lecture3.cs
+++ Lecture3/Lecture3.cs
@@ -69,3 +69,16 @@ string Method4(int count, string text)
 string res = Method4(10, "z ");
 Console.WriteLine(res);
 */
+
+//Пример №2
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} X {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
+
+
