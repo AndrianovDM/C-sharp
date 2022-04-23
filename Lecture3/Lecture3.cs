742847a773c9672472563e9c6f04cecca55b6da9
Dmitry <andrianov1997@mail.ru>
2022-04-23 16:38:51 +0300

    add Example 1

--- Lecture3/Lecture3.cs
+++ Lecture3/Lecture3.cs
@@ -54,3 +54,18 @@ string Method4(int count, string text)
 string res = Method4(10, "z ");
 Console.WriteLine(res);
 */
+
+//Пример №1
+/*
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z ");
+Console.WriteLine(res);
+*/
