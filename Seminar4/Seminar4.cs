76126152e9ce4c6c4681efbef22854990ae69dbb
Dmitry <andrianovdm1997@mail.ru>
2022-04-27 23:52:08 +0300

    add task 1

--- Seminar4/Seminar4.cs
+++ Seminar4/Seminar4.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №1*/
+
+int NaturalSum(int a)
+{
+    int sum = 0;
+    if (a < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i = 1; i <= a; i++)
+        {
+           sum += i;
+
+        }
+    return sum;
+    }
+}
+
+Console.WriteLine("Input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Sum elementis: " + NaturalSum(num));
