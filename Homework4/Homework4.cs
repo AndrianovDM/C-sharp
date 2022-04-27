8143484a8cf102493bc9af17b1e8d63c43ea5af4
Dmitry <andrianovdm1997@mail.ru>
2022-04-28 02:23:56 +0300

    add task 1

--- Homework4/Homework4.cs
+++ Homework4/Homework4.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача №34
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random ().Next(min, max);
+    }
+
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for (int i =0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FindNumberOfEven(int [] array)
+{
+    int even = 0;
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {   
+            even ++;
+        }
+    }
+    Console.WriteLine("Number of even elements -> " + even);
+}
+
+int[] myArray = CreateRandomArray(4, 100, 1000);
+ShowArray(myArray);
+FindNumberOfEven(myArray);
