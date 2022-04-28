016677ffa6a99bfc2bc520dfc5ee4d0ef7c25ffe
Dmitry <andrianovdm1997@mail.ru>
2022-04-29 00:07:03 +0300

    add Exemple 1

--- Lecture4/Lecture4.cs
+++ Lecture4/Lecture4.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Пример №1*/
+
+
+
+string[,] table = new string [2,5];
+table [1,2] = "Слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"--{table[rows,columns]}--");
+    }
+}
+
+
