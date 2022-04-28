da0bbb601e5f65c98b4a02cc51c8b11c31ef0e40
Dmitry <andrianovdm1997@mail.ru>
2022-04-29 00:56:35 +0300

    add Exemple 2

--- Lecture4/Lecture4.cs
+++ Lecture4/Lecture4.cs
@@ -1,7 +1,5 @@
 ﻿/* Пример №1*/
-
-
-
+/* 
 string[,] table = new string [2,5];
 table [1,2] = "Слово";
 
@@ -12,5 +10,38 @@ for (int rows = 0; rows < 2; rows++)
         Console.WriteLine($"--{table[rows,columns]}--");
     }
 }
+*/
+
+/* Пример №2*/
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j <  matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+       for (int j = 0; j < matr.GetLength(1); j++)
+       {
+           matr [i, j] = new Random ().Next(1,10);
+       } 
+    }
+}
+
+int [,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
 
 
