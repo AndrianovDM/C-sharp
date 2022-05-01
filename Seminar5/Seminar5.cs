d26dc19e9848252a5d6cc67c8eecefb38bf47469
Dmitry <andrianovdm1997@mail.ru>
2022-05-01 18:47:29 +0300

    add task 1

--- Seminar5/Seminar5.cs
+++ Seminar5/Seminar5.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №1*/
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random ().Next(min, max);
+    }
+
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for (int i =0; i< array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int [] ChangeNums(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= (-1);  
+    }
+    return array;
+}
+
+int[] myArray = CreateRandomArray(4, 1, 10);
+ShowArray(myArray);
+Console.WriteLine();
+int[] array = ChangeNums(myArray);
+ShowArray(myArray);
+
