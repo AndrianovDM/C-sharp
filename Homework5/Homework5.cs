6e686ba8566aad7e1c0ad54e76066a0b7c00abfd
Dmitry <andrianovdm1997@mail.ru>
2022-05-02 18:16:54 +0300

    add task 2

--- Homework5/Homework5.cs
+++ Homework5/Homework5.cs
@@ -3,7 +3,7 @@
 Напишите цикл, который принимает на вход два числа (A и B)
 и возводит число A в натуральную степень B.
 */
-
+/*
 int degreeOfNumber(int A, int B)
 {
     int num = A;
@@ -27,3 +27,32 @@ Console.Write("Input degree: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 int number = degreeOfNumber(numA,numB);
 Console.Write(number);
+*/
+
+/* Задача №2 */
+/*
+Напишите программу, которая принимает на 
+вход число и выдаёт сумму цифр в числе.
+*/
+
+int getNumber(int num)
+{
+    int number = num%10;
+    return number;
+}
+
+int sumNumber(int num)
+{   
+    int count = 0;
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += getNumber(num);
+        num = num / 10;
+        count++;
+    }
+    return sum;
+}
+Console.WriteLine("Input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum number: " + sumNumber(num));
