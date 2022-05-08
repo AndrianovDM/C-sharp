493cdfe28774ddc11bf3bda371b168cdee4699fd
Dmitry <andrianovdm1997@mail.ru>
2022-05-09 01:11:30 +0300

    add task 2

--- Seminar6/Seminar6.cs
+++ Seminar6/Seminar6.cs
@@ -1,5 +1,5 @@
 ﻿/*Задача №1*/
-
+/*
 bool IsTriangleExist(int a, int b, int c)
 {
     if (a < b + c && b < a + c && c < a + b)
@@ -17,4 +17,40 @@ else
 {
     Console.WriteLine("No");
 }
+*/
+/*Задача №2*/
+/* Развернуть массив */
 
+int [] CreateRandomArray(int size, int min, int max)
+{
+    int [] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+}
+
+int [] NewArray (int[] array)
+{
+    int newSize = array.Length;
+    int[] newArray = new int[newSize];
+    for (int i = 0, j = array.Length - 1; i< newSize; i++, j--)
+    {
+        newArray[i] = array[j];
+    }
+    return newArray;
+}
+
+int [] myArray = CreateRandomArray(10,0,10);
+int [] newArr = NewArray(myArray);
+ShowArray(myArray);
+Console.WriteLine();
+ShowArray(newArr);
