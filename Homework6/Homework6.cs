eefcf3bb7de1d0d29b9ed296f7124a92f968a130
Dmitry <andrianovdm1997@mail.ru>
2022-05-10 02:14:53 +0300

    add task 3

--- Homework6/Homework6.cs
+++ Homework6/Homework6.cs
@@ -37,7 +37,7 @@ CounterPositivUmber(myArray);
 Напишите программу, 
 которая будет преобразовывать десятичное число в двоичное.
 */
-
+/*
 int Counter(int number)
 {   
     int i = 0;
@@ -89,4 +89,34 @@ int[] arr = Binary(num, count);
 int[] newArr = Revers(arr);
 int binaryNumber = ShowNumber(newArr);
 Console.WriteLine(binaryNumber);
+*/
+
+/*Задача 43*/
+/*
+Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+void IntersectionPoint(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 -b1) / (k1 -k2);
+    double y =  k2 * x + b2;
+    Console.Write("(" + x + "," + y + ")");
+}
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+IntersectionPoint(k1,b1,k2,b2);
+
+
 
