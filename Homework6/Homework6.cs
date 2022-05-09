cd78314768e06e02b6770b0f54ad2a68bf034aa7
Dmitry <andrianovdm1997@mail.ru>
2022-05-09 20:16:02 +0300

    add task 2

--- Homework6/Homework6.cs
+++ Homework6/Homework6.cs
@@ -1,7 +1,7 @@
 ﻿/* Задача 41 */
 /*Пользователь вводит с клавиатуры M чисел. 
 Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
-
+/*
 int[] CreateArray(int size)
 {
     int[] array = new int[size];
@@ -30,4 +30,63 @@ Console.Write("How many numbers do you want to enter: ");
 int len = Convert.ToInt32(Console.ReadLine());
 int[] myArray = CreateArray(len);
 CounterPositivUmber(myArray);
+*/
+
+/* Задача 42 */
+/*
+Напишите программу, 
+которая будет преобразовывать десятичное число в двоичное.
+*/
+
+int Counter(int number)
+{   
+    int i = 0;
+    while (number >= 1)
+    {   
+        number = number / 2;
+        i++; 
+    }
+    return i;
+}
+
+int[] Binary(int number, int size)
+{
+    int[] array = new int[size];
+    int i = 0;
+    while(number >= 1)
+    {
+        array[i] = number % 2;
+        number = number / 2;
+        i++;
+    }
+    return array;
+}
+
+int [] Revers (int[] array)
+{
+    int newSize = array.Length;
+    int[] newArray = new int[newSize];
+    for (int i = 0, j = array.Length - 1; i < newSize; i++, j--)
+    {
+        newArray[i] = array[j];
+    }
+    return newArray;
+}
+
+int ShowNumber(int[] array)
+{
+    string bin = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+        bin = bin + Convert.ToString(array[i]);
+    int numberBin = Convert.ToInt32(bin);
+    return numberBin;
+}
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = Counter(num);
+int[] arr = Binary(num, count);
+int[] newArr = Revers(arr);
+int binaryNumber = ShowNumber(newArr);
+Console.WriteLine(binaryNumber);
 
