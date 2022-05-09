4dd0583000f1562792d3b5f139c93b032cb5df38
Dmitry <andrianovdm1997@mail.ru>
2022-05-09 18:28:14 +0300

    add task 1

--- Homework6/Homework6.cs
+++ Homework6/Homework6.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 41 */
+/*Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[i] = num;
+    }
+    return array;
+}
+
+void CounterPositivUmber(int [] array)
+{   
+    int count = 0;
+    for (int i = 0; i< array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.Write("Number of positive numbers: " + count);
+}
+
+Console.Write("How many numbers do you want to enter: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(len);
+CounterPositivUmber(myArray);
+
