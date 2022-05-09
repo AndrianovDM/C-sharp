8af266e8483429bcee90ffa529d30203f2d51083
Dmitry <andrianovdm1997@mail.ru>
2022-05-10 02:15:25 +0300

    Homework6 finish

--- Homework6/Homework6.cs
+++ Homework6/Homework6.cs
@@ -96,7 +96,7 @@ Console.WriteLine(binaryNumber);
 Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 */
-
+/*
 void IntersectionPoint(double k1, double b1, double k2, double b2)
 {
     double x = (b2 -b1) / (k1 -k2);
@@ -117,6 +117,7 @@ Console.Write("Input k2: ");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
 IntersectionPoint(k1,b1,k2,b2);
+*/
 
 
 
