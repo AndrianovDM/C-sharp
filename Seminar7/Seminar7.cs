5e458eac485cfe5f25be6106bd022e525c1d1924
Dmitry <andrianovdm1997@mail.ru>
2022-05-15 16:20:34 +0300

    add task 1

--- Seminar7/Seminar7.cs
+++ Seminar7/Seminar7.cs
@@ -1,1 +1,29 @@
-﻿
+﻿/*Задача №1*/
+
+int SumOfDiagonal(int[,] quad)
+{
+    int sum = 0;
+    for (int i = 0; i < quad.GetLength(0); i++)
+    {
+        sum += quad[i,i];
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Input size of matrix: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[size, size];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine(SumOfDiagonal(matrix));
