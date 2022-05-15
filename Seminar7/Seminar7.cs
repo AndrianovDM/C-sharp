5b28b2af00916a1ff1b362f2e1834ba92bc02c0c
Dmitry <andrianovdm1997@mail.ru>
2022-05-15 17:29:32 +0300

    add task 3

--- Seminar7/Seminar7.cs
+++ Seminar7/Seminar7.cs
@@ -29,7 +29,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 Console.WriteLine(SumOfDiagonal(matrix));
 
 /*Задача №2*/
-
+/*
 int [,] CreateArray(int size1, int size2)
 {
     int[,] array = new int [size1,size2];
@@ -45,4 +45,47 @@ int [,] CreateArray(int size1, int size2)
     return array;
 }
 CreateArray(3,3);
+*/
+
+/*Задача №3*/
+
+int [,] CreateArray2(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i+=2)
+    {
+        for (int j =0; j < array.GetLength(1); j+=2)
+        {
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+void ShowArray(int [,] matrix)
+{
+    for (int i =0; i< matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3,3];
+for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        matrix[i,j] = new Random().Next(0,10);
+
+ShowArray(matrix);
+Console.WriteLine();
+ShowArray(CreateArray2(matrix));
+
+
+
+
+
 
