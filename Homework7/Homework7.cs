c0ac6a83d9a5210a60b42b6caa1681a63ffbb0c6
Dmitry <andrianovdm1997@mail.ru>
2022-05-17 06:02:08 +0300

    add task 2

--- Homework7/Homework7.cs
+++ Homework7/Homework7.cs
@@ -2,7 +2,7 @@
 Задача №47
 Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 */
-
+/*
 double[,] CreateArray(int m, int n)
 {   
     double[,] array = new double[m,n];
@@ -35,4 +35,40 @@ int size2 = Convert.ToInt32(Console.ReadLine());
 
 double[,] matrix = CreateArray(size1,size2);
 ShowArray(matrix);
+*/
+
+/*
+Задача 50. 
+Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+int[,] matrix = new int[4, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("Input find vertical element m > 0:");
+int m = Convert.ToInt32(Console.ReadLine()) -1;
+Console.WriteLine("Input find horizontal element n > 0:");
+int n = Convert.ToInt32(Console.ReadLine()) -1;
+
+if (matrix.GetLength(0) >= m && matrix.GetLength(1) >= n)
+    {
+        Console.WriteLine("Element found: " + matrix[m, n]);
+    }
+else 
+{
+    Console.WriteLine("Element NOT found");
+}
+
+
+
+
 
