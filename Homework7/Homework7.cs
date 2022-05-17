e84f93e071d62a3b6f705dde6e29de0409d92a83
Dmitry <andrianovdm1997@mail.ru>
2022-05-17 07:36:01 +0300

    Homework7 finish

--- Homework7/Homework7.cs
+++ Homework7/Homework7.cs
@@ -74,7 +74,7 @@ else
 Задайте двумерный массив из целых чисел. 
 Найдите среднее арифметическое элементов в каждом столбце.
 */
-
+/*
 int[,] matrix = new int[4, 4];
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
@@ -95,6 +95,7 @@ for (int i = 0; i < matrix.GetLength(1); i++)
     }
     Console.Write(arithmeticMean/4 + ", ");
 }
+*/
 
 
 
