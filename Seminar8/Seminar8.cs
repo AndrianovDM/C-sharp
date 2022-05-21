541d9b12eafd771f5bf109dc72892d1576d4ec17
Dmitry <andrianovdm1997@mail.ru>
2022-05-21 21:04:24 +0300

    add task 3

--- Seminar8/Seminar8.cs
+++ Seminar8/Seminar8.cs
@@ -50,7 +50,7 @@ PrintArray(ChangeRowColor(arr));
 */
 
 /* Задача №2 */
-
+/*
 int[,] CreateArray(int n , int m)
 {
     int [,] array = new int [n, m];
@@ -91,6 +91,71 @@ int[,] ChangeRowColor(int[,] matrix)
         }
     }
     return result;
+}
+
+Console.Write("Input matrix width: ");
+int width = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input matrix hight: ");
+int hight = Convert.ToInt32(Console.ReadLine());
+int [,] arr = CreateArray(hight, width);
+
+if (hight == width)
+{
+    PrintArray(ChangeRowColor(arr));;
+}
+else
+{
+    Console.WriteLine(" Cannot change row to column. Matrix is not square");
+}
+*/
+
+/* Задача №3 */
+
+int[,] CreateArray(int n , int m)
+{
+    int [,] array = new int [n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            array[i,j] = new Random ().Next(-10,11);
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+void PrintArray(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeRowColor(int[,] matrix)
+{
+    int tmp = 0;
+    int k = matrix.GetLength(0) - 1;
+    int l = matrix.GetLength(1) - 1;
+
+
+    for (int i = 0; i <= k; i++)
+    {
+        for (int j = i + 1; j <= l ; j++)
+        {
+            tmp = matrix[i , j];
+            matrix[i, j] = matrix[j , i];
+            matrix[j, i] = tmp;
+        }
+    }
+    return matrix;
 }
 
 Console.Write("Input matrix width: ");
