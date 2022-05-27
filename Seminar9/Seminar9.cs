50794f09a2cdbe84389e36d45742a867f51f4dcd
Dmitry <andrianovdm1997@mail.ru>
2022-05-28 02:52:54 +0300

    add task 2

--- Seminar9/Seminar9.cs
+++ Seminar9/Seminar9.cs
@@ -1,5 +1,5 @@
 ﻿/* Задача№1 */
-
+/*
 NaturalNums(5);
 
 void NaturalNums(int n)
@@ -10,3 +10,19 @@ void NaturalNums(int n)
         NaturalNums(n-1);
     }
 }
+*/
+
+/* Задача№2 */
+
+int TestRecurse(int n , int m)
+{
+    if (m >= 1)
+    {
+        m-= 1;
+        return n * TestRecurse(n, m);
+    }
+    else
+        return 1;
+}
+
+Console.Write(TestRecurse(2,4));
