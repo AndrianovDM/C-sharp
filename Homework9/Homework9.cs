26fa4625ca454eed3196cb5f127215cd6ca058bb
Dmitry <andrianovdm1997@mail.ru>
2022-05-28 15:39:31 +0300

    add task 1

--- Homework9/Homework9.cs
+++ Homework9/Homework9.cs
@@ -1,1 +1,22 @@
-﻿
+﻿/* Задача №66 */
+/*
+Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+*/
+
+int SumNumber(int m, int n)
+{
+    if (m <= n)
+    {
+        return m + SumNumber(m + 1, n);
+    }
+    else
+        return 0;
+
+}
+Console.WriteLine("Input number start: ");
+int numStart = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number finish: ");
+int numFinish = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum of numbers: ");
+Console.Write(SumNumber(numStart, numFinish));
