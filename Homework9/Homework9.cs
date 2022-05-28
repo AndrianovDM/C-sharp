ee7b6ceafc909cbdf40d6633f5cc176939158a0b
Dmitry <andrianovdm1997@mail.ru>
2022-05-28 16:05:11 +0300

    add task 2

--- Homework9/Homework9.cs
+++ Homework9/Homework9.cs
@@ -3,7 +3,7 @@
 Задайте значения M и N. 
 Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 */
-
+/*
 int SumNumber(int m, int n)
 {
     if (m <= n)
@@ -20,3 +20,22 @@ Console.WriteLine("Input number finish: ");
 int numFinish = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Sum of numbers: ");
 Console.Write(SumNumber(numStart, numFinish));
+*/
+
+/* Задача №67 */ 
+/* Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр. */
+
+int NumberSum(int m)
+{
+    if (m == 0)
+    {   
+        return 0;
+    }
+    else
+        return m % 10 + NumberSum(m/10);
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum of numbers: ");
+Console.Write(NumberSum(num));
