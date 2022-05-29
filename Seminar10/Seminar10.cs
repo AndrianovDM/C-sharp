f19d3d7499ec05fa973b48e1683a0eb9a8f2b94c
Dmitry <andrianovdm1997@mail.ru>
2022-05-29 22:31:35 +0300

    add task 1

--- Seminar10/Seminar10.cs
+++ Seminar10/Seminar10.cs
@@ -1,1 +1,15 @@
-﻿
+﻿/* Задача № 1 */
+
+int CountEven(string[] words)
+{
+    int count = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length % 2 == 0) count++;
+    }
+    return count;
+}
+
+string[] names = {"Pavel", "Anna", "Denis"};
+
+Console.WriteLine(CountEven(names));
