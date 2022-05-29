04fbe5efca785f0bd7864e8ad514675ce80e18e8
Dmitry <andrianovdm1997@mail.ru>
2022-05-29 03:22:44 +0300

    add task 1

--- Lecture5/Lecture5.cs
+++ Lecture5/Lecture5.cs
@@ -1,1 +1,26 @@
-﻿
+﻿/* Задача №1 */
+
+// без применнения рекурсии
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i >= b; i--)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+Console.WriteLine(NumbersRec(1, 10));
+
+// с помощью рекурсии
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+ else return String.Empty;
+}
+Console.WriteLine(NumbersFor(1, 10)); 
+ 
+
+
+
+
