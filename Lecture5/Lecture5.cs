109f40c56725516fd01cad999ff6779f20e4a1fa
Dmitry <andrianovdm1997@mail.ru>
2022-05-29 14:41:25 +0300

    add task 10

--- Lecture5/Lecture5.cs
+++ Lecture5/Lecture5.cs
@@ -156,7 +156,7 @@ CatalogInfo(path);
 */
 
 /* Задача №9 */
-
+/*
 void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
 {
  if (count > 1) Towers(with, some, on, count - 1);
@@ -165,5 +165,25 @@ void Towers(string with = "1", string on = "3", string some = "2", int count = 3
 }
 
 Towers();
+*/
+
+/* Задача №10 */
+
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// 0 1 2 3 4 5 6 7 8 9 10 11
+void InOrderTraversal(int pos = 1)
+{
+ if (pos < tree.Length)
+ {
+ int left = 2 * pos;
+ int right = 2 * pos + 1;
+ if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+ Console.WriteLine(tree[pos]);
+ if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+ }
+}
+
+InOrderTraversal(2);
 
 
