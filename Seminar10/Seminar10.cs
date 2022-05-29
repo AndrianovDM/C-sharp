665ec14a36e32917d383bc9e77a5c215754c595d
Dmitry <andrianovdm1997@mail.ru>
2022-05-30 02:07:28 +0300

    add task 2

--- Seminar10/Seminar10.cs
+++ Seminar10/Seminar10.cs
@@ -1,5 +1,5 @@
 ﻿/* Задача № 1 */
-
+/*
 int CountEven(string[] words)
 {
     int count = 0;
@@ -13,3 +13,50 @@ int CountEven(string[] words)
 string[] names = {"Pavel", "Anna", "Denis"};
 
 Console.WriteLine(CountEven(names));
+*/
+
+/* Задача № 2 */
+
+string[] names = {"Pavel", "Oleg", "Anna", "Nikita", "Max"};
+
+string[] SortArray(string[] words)
+{   
+    string temp = string.Empty;
+    for (int i = 0; i < words.Length - 1; i++)
+    {
+        for (int j = 0; j < words.Length - 1 -i; j++)
+        {
+            if (words[j].Length > words[j + 1].Length)
+            {
+                temp = words[j];
+                words[j] = words[j + 1];
+                words[j + 1] = temp;
+            }
+            else if (words[j].Length == words[j + 1].Length)
+            {
+                for (int k = 0; k < words[j].Length; k++)
+                {
+                    if (words[j][k] > words[j + 1][k])
+                    {
+                        temp = words[j];
+                        words [j] = words[j + 1];
+                        words [j + 1] = temp;
+                        break;
+                    }
+                    else if (words[j][k] < words[j + 1][k]) break;
+                }
+            }
+        }
+    }
+    return words;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+PrintArray(SortArray(names));
